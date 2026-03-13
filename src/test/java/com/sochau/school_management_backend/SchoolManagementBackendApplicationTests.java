89fef40a1943a7e4a57f0b6523f4107f51a2dc65
SudipDahal2 <80csi42@texasintl.edu.np>
2026-03-13 12:12:04 +0545

    Initialize Spring Boot backend

---
+++ src/test/java/com/sochau/school_management_backend/SchoolManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sochau.school_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
